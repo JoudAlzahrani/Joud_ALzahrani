97a2b1a9459d0f169e6dfaaa5ca9615e1aeed2bf
JoudAlzahrani <Joudxi1415@gmail.com>
2024-09-01 22:28:58 +0300

    NetBeans Project

---
+++ src/joud251/Joud251.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joud251;
+
+/**
+ *
+ * @author jood
+ */
+public class Joud251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Joud";
+        
+        if(name == "Joud")
+        System.out.println("She study 251");
+        else
+         System.out.println("She not study 521"); 
+        
+        
+    }
+    
+}
